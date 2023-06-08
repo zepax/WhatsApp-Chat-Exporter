695702980f8971c1db584571c53a1dc6e575cbab
KnugiHK <24708955+KnugiHK@users.noreply.github.com>
2023-06-08 18:16:47 +0800

    Implement splitted outputs #23

--- Whatsapp_Chat_Exporter/__main__.py
+++ Whatsapp_Chat_Exporter/__main__.py
@@ -126,9 +126,13 @@ def main():
     parser.add_argument(
         "--size",
         "--output-size",
+        "--split",
         dest="size",
+        nargs='?',
+        type=int,
+        const=0,
         default=None,
-        help="Maximum size of a single output file in bytes, 0 for auto (not yet implemented)"
+        help="Maximum (Rough) size of a single output file in bytes, 0 for auto"
     )
     parser.add_argument(
         "--no-html",
@@ -217,7 +221,10 @@ def main():
     elif args.iphone:
         import sys
         if "--iphone" in sys.argv:
-            print("WARNING: The --iphone flag is deprecated and will be removed in the future. Use --ios instead.")
+            print(
+                "WARNING: The --iphone flag is deprecated and will"
+                "be removed in the future. Use --ios instead."
+            )
         messages = extract_iphone.messages
         media = extract_iphone.media
         vcard = extract_iphone.vcard
@@ -272,7 +279,7 @@ def main():
                     shutil.move(args.media, f"{args.output}/")
                 except PermissionError:
                     print("Cannot remove original WhatsApp directory. "
-                        "Perhaps the directory is opened?")
+                          "Perhaps the directory is opened?")
 
     if options.json:
         data = {jik : chat.to_json() for jik,chat in data.items()}
