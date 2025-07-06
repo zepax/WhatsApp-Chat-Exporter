6acc9d80b888e48671ea2c6a72d57e2aa50f9116
zepax <66538734+zepax@users.noreply.github.com>
2025-07-06 12:15:24 -0700

    Add global phone number support

--- Whatsapp_Chat_Exporter/vcards_contacts_test.py
+++ Whatsapp_Chat_Exporter/vcards_contacts_test.py
@@ -1,10 +1,21 @@
 # from contacts_names_from_vcards import readVCardsFile
 
+import tempfile
+import os
+
 from Whatsapp_Chat_Exporter.vcards_contacts import normalize_number, read_vcards_file
 
 
 def test_readVCardsFile():
-    assert len(read_vcards_file("contacts.vcf", "973")) > 0
+    sample_vcf = """BEGIN:VCARD\nFN:John Doe\nTEL:+1234567890\nEND:VCARD\n"""
+    with tempfile.NamedTemporaryFile(mode='w+', delete=False) as tmp:
+        tmp.write(sample_vcf)
+        path = tmp.name
+    try:
+        result = read_vcards_file(path, "1")
+        assert result == [("1234567890", "John Doe")]
+    finally:
+        os.unlink(path)
 
 def test_create_number_to_name_dicts():
     pass
