d15799155f8aca629af6c8d00b13d626f3f2f936
zepax <66538734+zepax@users.noreply.github.com>
2025-07-07 04:09:24 -0700

    Add Typer CLI and progress bar

---
+++ Whatsapp_Chat_Exporter/test_cli.py
@@ -0,0 +1,18 @@
+from typer.testing import CliRunner
+from Whatsapp_Chat_Exporter.cli import app
+
+
+runner = CliRunner()
+
+
+def test_cli_help():
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+    assert "Usage: export" in result.output
+
+
+def test_cli_help_export():
+    result = runner.invoke(app, ["export", "--help"])
+    assert result.exit_code == 0
+    assert "--android" in result.output
+
